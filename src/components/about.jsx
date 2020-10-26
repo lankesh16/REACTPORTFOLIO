501e46f0ddf93e84ab3693b96da36f982813a7f4
Lankesh <lankeshdesilva16@gmail.com>
2020-10-25 23:29:53 -0400

    pushing final hmk

---
+++ src/components/about.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-12">
+                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                <div className="col-md-12">
+                    <div className="about-desc">
+                    <span className="heading-meta">About Me</span>
+                    <h2 className="colorlib-heading">Hi I'm Lankesh</h2>
+                    <p>I am a web developer. I am completing a web developer program at Carlton University. I am very passionat and love to code. I work hard and always stay productive. I bring a variety of skills, including leadership skills and strong clerical background. I am also very creative. I have created many different applications. Welcome to my portfolio page.</p>
+                    <p>Please take the time to review my portfolio, and feel free to contact me at anytime. I am always looking to collaborate on new projects.</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+        <section className="colorlib-about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">What I do?</span>
+                <h2 className="colorlib-heading">Here are some of my skills</h2>
+            </div>
+            </div>
+            <div className="row row-pt-md">
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-1">
+                <span className="icon">
+                    <i className="icon-bulb" />
+                </span>
+                <div className="desc">
+                    <h3>Web Development </h3>
+                    <p>I have experience building websites using JavaScript,React,HTML,CSS</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-3">
+                <span className="icon">
+                    <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                    <h3>Data Structures</h3>
+                    <p>I have a strong background in mysql and mongodb. </p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-5">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>Web framework</h3>
+                    <p>I am also very familiar with react.js and node.js</p>
+                </div>
+                </div>
+            </div>
+           
+            </div>
+        </div>
+        </section>
+      </div>
+    )
+  }
+}
---
+++ src/components/timeline.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Web developement -Carleton University <span>2020-present</span></h2>
+                        <p>I am currently attending a coding program through trilogy backed by Carleton University. I have acquire alot of knowledge about web development through this program. I can proficiently make professional websites using different frameworks.I have learned alot about back end coding, and dealing with databases.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Avanced Diploma -Child & Youth Care<span>2014-2017</span></h2>
+                <p> I have graduted from Algonquin College in Community and health services. I have learned many translatable skills. I have extremely strong social skills and I have come to understand the importance of risk management, team organization, time management etc.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Front Office Supervisor - Hilton Garden Inn <span>2016-2020</span></h2>
+                        <p> I have very strong leadership skills. I understand the challenges of working in a team and managing a team, meeting deadlines, and working together. I am an effective leader and always welcome a challenge. I work diligently and persitently. </p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
